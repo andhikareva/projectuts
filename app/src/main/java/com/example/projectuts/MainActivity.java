049b7b145e1c059db0ae0079da219ac3bf75f022
andhikareva <andhikaprtm4@gmail.com>
2020-04-02 11:26:05 +0700

    progress

--- app/src/main/java/com/example/projectuts/MainActivity.java
+++ app/src/main/java/com/example/projectuts/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.projectuts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void handleRM(View view) {
+        Intent intent = new Intent(this, RMActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleCFC(View view) {
+        Intent intent = new Intent(this, CFCActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleFCB(View view) {
+        Intent intent = new Intent(this, FCBActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleArema(View view) {
+        Intent intent = new Intent(this, AremaActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleExit(View view) {
+        finish();
     }
 }
